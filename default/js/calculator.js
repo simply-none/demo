210619cc36319ca903ef58895c0f30f2e6c42706
yuyuanqiu <jousindea@qq.com>
2019-04-17 16:30:54 +0800

    calculate modify

--- default/js/calculator.js
+++ default/js/calculator.js
@@ -7,14 +7,32 @@ function power(num,index) {
     return num ** index;
 }
 // 获取计算器主盘的数字
-function getValue() {
-    var getNumberString = 'a';
-    var getTd = document.getElementsByTagName("td");
-    for (var i = 0; i < getTd.length; i++) {
-        getTd[i].onclick = function() {
-            document.getElementById("calculate").innerHTML = (this.innerHTML);
-        };
-    }
-
+var getNumberString = '';
+// function getValue() {
+var getTd = document.getElementsByTagName("td");
+var getDiv = document.getElementById("calculate");
+// 获取主盘上所有的键的值，并显示在计算栏
+for (var i = 0; i < getTd.length; i++) {
+	getTd[i].onclick = function() {
+		if (this.innerHTML === "x<sup>2</sup>") {
+			getNumberString += "<sup>2</sup>"
+		}
+		if (this.innerHTML === "x<sup>y</sup>") {
+			getNumberString += "^"
+		}
+		else {
+			getNumberString += this.innerHTML;    
+        	
+		}
+		getDiv.innerHTML = getNumberString;
+    };
 }
-getValue();
+    // alert(getNumberString);
+// }
+
+// 清除内容
+var deleteAll = document.getElementById("deleteAll");
+deleteAll.onclick = function() {
+	getNumberString = '';
+	getDiv.innerHTML = getNumberString;
+}
